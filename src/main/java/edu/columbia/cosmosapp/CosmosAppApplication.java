a44f397ca26442f16427b9a8472b6c8e4d1df4f0
Jiawei Lu <jl5999@columbia.edu>
2023-10-04 17:58:28 -0400

    0.0.1-snapshot

---
+++ src/main/java/edu/columbia/cosmosapp/CosmosAppApplication.java
@@ -0,0 +1,13 @@
+package edu.columbia.cosmosapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmosAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CosmosAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/columbia/cosmosapp/CosmosAppApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.columbia.cosmosapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmosAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
